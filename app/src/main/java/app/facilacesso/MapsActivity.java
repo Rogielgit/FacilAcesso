d8c2f0a8f7271489fbac0fde7c98037ab8afcd18
Rogiel <rogiel2009@gmail.com>
2017-10-24 23:45:23 -0200

    Maps works

--- app/src/main/java/app/facilacesso/MapsActivity.java
+++ app/src/main/java/app/facilacesso/MapsActivity.java
@@ -39,8 +39,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng posicao = new LatLng(-22.0005881, -47.898990);
+        mMap.addMarker(new MarkerOptions().position(posicao).title("Sua posicao"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(posicao));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posicao, 15));
+
     }
 }
