a38a99f6b0070c00f13b053291200b7c01774b5f
Rogiel <rogiel2009@gmail.com>
2017-12-04 00:31:06 -0200

    add initial screen

---
+++ app/src/main/java/app/facilacesso/InitialScreen.java
@@ -0,0 +1,27 @@
+package app.facilacesso;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by computeiro on 03/12/17.
+ */
+
+public class InitialScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initialscreen);
+
+        int secondsDelayed = 4;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(InitialScreen.this,  MapsActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+}
